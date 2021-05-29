b4878039202a174cedecf37950a5357319c93a25
Marc <marc82490@gmail.com>
2021-05-29 14:04:21 -0500

    Add skeleton code for game classes

---
+++ chess.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Game {
+    public:
+
+    private:
+};
+
+class Board {
+    public:
+
+    private:
+};
+
+class Piece {
+    public:
+
+    private:
+};
+
+class Player {
+    public:
+
+    private:
+};
+
+int main () {
+
+}
